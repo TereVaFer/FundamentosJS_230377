4d343ad31372f614afed0fd6a2bb3b5a085cf340
Teresa Vargas Fernández <230377@utxicotepec.edu.mx>
2024-09-17 16:39:48 -0600

    Practica 04
    Agregación se String y BigInt al Ejercicio 02

--- js/Ejercicio_02.js
+++ js/Ejercicio_02.js
@@ -60,3 +60,29 @@ console.log(`Tu nuevo saldo es de:  ${saldo_cuenta}`);
 monto_trasaccion=1500; 
 console.log(`Tu abono de : ${monto_trasaccion} ha sido recibido, tu nuevo saldo es de : ${(saldo_cuenta+monto_trasaccion)}`) ;
 
+
+
+//Strings o cadenas de texto
+const alumno = "Teresa Vargas Fernández"
+let producto;
+
+console.warn("--- Tipo de dato - String (Cadena o caractéres");
+console.log(`El nombre del alumno es : ${alumno}, que es un tipo de dato ${typeof alumno}`)
+producto = "Monitor 20 pulgadas"
+console.log(`El nombre del producto es : ${prducto}, que es un tipo de dato ${typeof producto}`)
+
+//Manipulando los Strings
+console.log(`Más adelante podremos transformar el contenido de los String usando funciones especificas como convertir a mayusculas: ${alumno} => ${alumno.toUpperCase()}`)
+console.log(`o en su defecto a minusculas: ${producto} => ${producto.toLowerCase()}`);
+
+//BigInt (Entero de Dimensiones Amplias) - Generalmente utilizado en sistemas matemáticos físicos o especiales donde se requiere de una excelente precición numerica
+console.warn("---Tipo de Dato - BigInt (Número amplio)");
+const numeroGrande = 1234567890
+const numeroGrande2 = 12345678901234567890
+const numeroGrande3 = 123456789012345678901234567890
+const numeroGrande4 = 1234567890123456789012345678901234567890
+
+console.log(`El primer experimento de un numero grande es ${numeroGrande}, que si es soportado por NUMBER, y su tipoo de dato es: ${typeof(numeroGrande)}`)
+console.log(`El primer experimento de un numero grande es ${numeroGrande2}, que si es soportado por NUMBER, y su tipoo de dato es: ${typeof(numeroGrande2)}`)
+console.log(`El primer experimento de un numero grande es ${numeroGrande3}, que si es soportado por NUMBER, y su tipoo de dato es: ${typeof(numeroGrande3)}`)
+console.log(`El primer experimento de un numero grande es ${numeroGrande4}, que si es soportado por NUMBER, y su tipoo de dato es: ${typeof(numeroGrande4)}`)
